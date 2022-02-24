d11385d5a2d2bde0dd395c280559bfbda0ad2583
Mauricio <mauriciomonta363@gmail.com>
2022-02-24 01:59:35 -0300

    Script added for Mobile menu

--- script.js
+++ script.js
@@ -30,3 +30,30 @@ skillDropDown.addEventListener('click', () => {
     skillList.classList.toggle('showed');
   }, 500);
 });
+
+const hamBtn = document.getElementById('hamBtn');
+const mobileMenu = document.getElementById('links');
+const closeBtn = document.getElementById('hamBtnClose');
+const mobileLinks = document.getElementById('mobile-menu');
+
+hamBtn.addEventListener('click', () => {
+  // console.log('clicked');
+  mobileMenu.classList.add('visible');
+  setTimeout(() => {
+    mobileMenu.classList.add('showed');
+  }, 100);
+});
+
+closeBtn.addEventListener('click', () => {
+  mobileMenu.classList.remove('showed');
+  setTimeout(() => {
+    mobileMenu.classList.remove('visible');
+  }, 100);
+});
+
+mobileLinks.addEventListener('click', () => {
+  mobileMenu.classList.remove('showed');
+  setTimeout(() => {
+    mobileMenu.classList.remove('visible');
+  }, 100);
+});
